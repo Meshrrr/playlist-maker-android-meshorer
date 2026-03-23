0de07b1a67f1390cc91aad0d9051fc5e582dd8dd
Meshrr <volframhome@gmail.com>
2026-03-23 13:35:46 +0500

    refactor settings screen: add column with buttons, create ButtonSample func, add logic to share button and support button

--- app/src/main/java/com/practicum/playlist_maker_android_meshorer/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker_android_meshorer/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.practicum.playlist_maker_android_meshorer
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
@@ -8,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -24,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -73,77 +77,148 @@ fun SettingsScreen() {
 
             }
         )
+        Column(modifier = Modifier.padding(top = 80.dp).fillMaxWidth().height(244.dp)) {
 
-        Row(modifier = Modifier.fillMaxWidth().padding(top = 80.dp).height(61.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            content = {
-                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
-                    text = stringResource(id = R.string.black_theme),
-                    fontSize = 16.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    style = MaterialTheme.typography.bodyMedium)
+            ButtonSample(
+                button_description = stringResource(id = R.string.black_theme),
+                painter = painterResource(R.drawable.control_theme),
+                width_icon = 35,
+                height_icon = 18
+                ) { }
 
-                Image(modifier = Modifier.padding(end = 18.dp).width(35.dp).height(18.dp),
-                    painter = painterResource(id = R.drawable.control_theme),
-                    contentDescription = null)
-            })
+            ButtonSample(
+                button_description = stringResource(id = R.string.share_app),
+                painter = painterResource(R.drawable.share_icon),
+                width_icon = 24,
+                height_icon = 24
+            ) {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Download PlaylistMaker!")
+                shareIntent.setType("text/plain")
+                context.startActivity(shareIntent)
+            }
 
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(top = 141.dp).height(61.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            content = {
-                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
-                    text = stringResource(id = R.string.share_app),
-                    fontSize = 16.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    style = MaterialTheme.typography.bodyMedium)
+            ButtonSample(
+                button_description = stringResource(id = R.string.support),
+                painter = painterResource(R.drawable.support),
+                width_icon = 24,
+                height_icon = 24
+            ) {
+                val supportSendIntent = Intent(Intent.ACTION_SENDTO)
+                supportSendIntent.data = Uri.parse("mailto:")
+                supportSendIntent.putExtra(Intent.EXTRA_EMAIL,
+                    arrayOf(context.getString(R.string.mail)))
+                supportSendIntent.putExtra(Intent.EXTRA_SUBJECT,
+                    "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+                supportSendIntent.putExtra(Intent.EXTRA_TEXT,
+                    "Спасибо разработчикам и разработчицам за крутое приложение!")
 
-                Image(modifier = Modifier.padding(end = 12.dp).size(24.dp),
-                    painter = painterResource(id = R.drawable.share_icon),
-                    contentDescription = "Поделиться")
 
             }
-        )
 
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(top = 202.dp).height(61.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            content = {
-                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
-                    text = stringResource(id = R.string.support),
-                    fontSize = 16.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    style = MaterialTheme.typography.bodyMedium)
-
-                Image(modifier = Modifier.padding(end = 12.dp).size(24.dp),
-                    painter = painterResource(id = R.drawable.support),
-                    contentDescription = (R.string.support.toString()))
-
-            }
-        )
-
-        Row(
-            modifier = Modifier.fillMaxWidth().padding(top = 263.dp).height(61.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            content = {
-                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
-                    text = stringResource(id = R.string.agreement),
-                    fontSize = 16.sp,
-                    fontFamily = FontFamily.SansSerif,
-                    style = MaterialTheme.typography.bodyMedium)
-
-                Image(modifier = Modifier.padding(end = 20.dp).width(8.dp).height(14.dp),
-                    painter = painterResource(id = R.drawable.arrow_button),
-                    contentDescription = (R.string.support.toString()))
-
-            }
-        )
+            ButtonSample(
+                button_description = stringResource(id = R.string.agreement),
+                painter = painterResource(R.drawable.arrow_button),
+                width_icon = 18,
+                height_icon = 14
+            ) { }
+        }
+//        Row(modifier = Modifier.fillMaxWidth().padding(top = 80.dp).height(61.dp),
+//            verticalAlignment = Alignment.CenterVertically,
+//            content = {
+//                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
+//                    text = stringResource(id = R.string.black_theme),
+//                    fontSize = 16.sp,
+//                    fontFamily = FontFamily.SansSerif,
+//                    style = MaterialTheme.typography.bodyMedium)
+//
+//                Image(modifier = Modifier.padding(end = 18.dp).width(35.dp).height(18.dp),
+//                    painter = painterResource(id = R.drawable.control_theme),
+//                    contentDescription = null)
+//            })
+//
+//        Row(
+//            modifier = Modifier.fillMaxWidth().padding(top = 141.dp).height(61.dp),
+//            verticalAlignment = Alignment.CenterVertically,
+//            content = {
+//                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
+//                    text = stringResource(id = R.string.share_app),
+//                    fontSize = 16.sp,
+//                    fontFamily = FontFamily.SansSerif,
+//                    style = MaterialTheme.typography.bodyMedium)
+//
+//                Image(modifier = Modifier.padding(end = 12.dp).size(24.dp),
+//                    painter = painterResource(id = R.drawable.share_icon),
+//                    contentDescription = "Поделиться")
+//
+//            }
+//        )
+//
+//        Row(
+//            modifier = Modifier.fillMaxWidth().padding(top = 202.dp).height(61.dp),
+//            verticalAlignment = Alignment.CenterVertically,
+//            content = {
+//                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
+//                    text = stringResource(id = R.string.support),
+//                    fontSize = 16.sp,
+//                    fontFamily = FontFamily.SansSerif,
+//                    style = MaterialTheme.typography.bodyMedium)
+//
+//                Image(modifier = Modifier.padding(end = 12.dp).size(24.dp),
+//                    painter = painterResource(id = R.drawable.support),
+//                    contentDescription = (R.string.support.toString()))
+//
+//            }
+//        )
+//
+//        Row(
+//            modifier = Modifier.fillMaxWidth().padding(top = 263.dp).height(61.dp),
+//            verticalAlignment = Alignment.CenterVertically,
+//            content = {
+//                Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
+//                    text = stringResource(id = R.string.agreement),
+//                    fontSize = 16.sp,
+//                    fontFamily = FontFamily.SansSerif,
+//                    style = MaterialTheme.typography.bodyMedium)
+//
+//                Image(modifier = Modifier.padding(end = 20.dp).width(8.dp).height(14.dp),
+//                    painter = painterResource(id = R.drawable.arrow_button),
+//                    contentDescription = (R.string.support.toString()))
+//
+//            }
+//        )
     }
 }
 
 @Composable
-private fun ButtonSample() {
+private fun ButtonSample(button_description: String,
+                         painter: Painter,
+                         width_icon: Int,
+                         height_icon: Int,
+                         onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth().height(61.dp),
+        colors = ButtonDefaults.buttonColors(Color.White),
+        content = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                content = {
+                    Text(modifier = Modifier.padding(start = 16.dp).weight(1f),
+                        text = button_description,
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily.SansSerif,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Black)
 
+                    Image(modifier = Modifier.padding(end = 20.dp).width(width_icon.dp).height(height_icon.dp),
+                        painter = painter,
+                        contentDescription = button_description)
+                }
+            )
+        },
+    )
 }
 
 
